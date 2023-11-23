2794c7031c6c2a20a31efff140178e7e94f94f99
Hoang Duong <hoangduongphantri@gmail.com>
2023-11-23 18:56:50 +0200

    Add redux and tailwind settings

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      xs: '475px',
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
         red: {
